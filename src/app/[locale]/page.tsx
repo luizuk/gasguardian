3b6849bae30d74e9c3f3a41c2e2f2f19f2394e01
luizuk <luizuksouza@gmail.com>
2025-02-08 17:11:49 -0300

    refactor: remove unnecessary sections

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,30 +10,6 @@ export default function App() {
   return (
     <main className="flex w-full bg-gray-200 flex-col items-center min-h-screen pt-32">
       <Header />
-
-      {/* <Hero
-        button2Href={whatsAppLink}
-        button2Text={heroT("cta")}
-        ctaButtonLink={`/${locale}/login`}  
-        ctaButtonText={t("login")}
-        description={heroT("subtitle")}
-        title={heroT("title")}
-        topCtaText={heroT("topCtaText")}
-      />
-
-      <AISolutionsSection />
-
-      <CloudSection />
-
-      <ContactSection />
-
-      <FAQSection />
-
-      <LeadFormSection />
-
-      <Footer /> */}
-
-      {/* <WebsitesSection /> */}
     </main>
   );
 }
